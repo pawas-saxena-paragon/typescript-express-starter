bbaf1fb1203a0877be77d0026b7b537facb9ec83
pawas <pawas.saxena+1@gmail.com>
2024-01-09 20:38:15 +0530

    added a router

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import peopleRouter from './people/people.route'; 
+
 const port = 3000;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors());
+
+app.use('/people', peopleRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello service is up");
@@ -16,6 +20,7 @@ app.post("/", (req, res) => {
   console.log(req.body);
   res.send({ sucess: true });
 });
+
 
 app.listen(port, () => {
   console.log(`app listening on ${port}`);
